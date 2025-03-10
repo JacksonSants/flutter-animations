df29b261e7c356e7e8214ebfffdcf376a6a87f0a
Jackson Ribeiro <jacksonribeiro@bemol.com.br>
2025-03-10 10:55:49 -0400

    Desenvolvimento e implementação de animações usando a biblioteca Animations do flutter.

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:hyrule/screens/categories.dart';
+
+void main() {
+  runApp(const Hyrule());
+}
+
+class Hyrule extends StatelessWidget {
+  const Hyrule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hyrule',
+      theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: SharedAxisPageTransitionsBuilder(transitionType: SharedAxisTransitionType.horizontal),
+          TargetPlatform.iOS: SharedAxisPageTransitionsBuilder(transitionType: SharedAxisTransitionType.horizontal)
+        }), //Definiçãod de biblioteca Animations para transição de interface
+        colorSchemeSeed: Colors.blue,
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const Categories(),
+    );
+  }
+}
+
+
